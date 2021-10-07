254c099b071c534cea7e2489836521a22eeb6700
KamogeloMafaesa <60877078+kamogeloo@users.noreply.github.com>
2021-10-07 16:55:40 +0200

    changed background size

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ container.addEventListener("mousemove", (e) => {
 });
 
 container.addEventListener("touchstart", (e) =>{
-    let xAxis = (window.innerWidth /)
+    let xAxis = (window.innerWidth /2 - e.pageX)/15;
+    let yAxis = (window.innerHeight/2 - e.pageY)/15;
+
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 //animate in
 
@@ -34,6 +37,18 @@ container.addEventListener("mouseenter", (e)=>{
     }
     
 });
+//for phone
+container.addEventListener("touchmove",(e) =>{
+    container.style.filter = "drop-shadow(1px 2px 10px red)";
+    container.style.transform = "translateZ(250px)";
+    
+    //change color of illustration
+    let ryan = document.querySelectorAll(".ryan");
+    let i;
+    for(i = 0; i< ryan.length; i++){
+        ryan[i].style.backgroundColor = "red";
+    }
+});
 
 //animate out
 container.addEventListener("mouseleave", (e) =>{
@@ -53,4 +68,18 @@ container.addEventListener("mouseleave", (e) =>{
 
     
 });
+container.addEventListener("touchend", (e)=>{
+    card.style.transition = 'all 0.5s ease';
+    card.style.transform= `rotateY(0deg) rotateX(0deg)`;
+
+    //pop back
+    container.style.transform = "translateZ(0px)";
+    container.style.filter = 'none';
+    
+    let ryan = document.querySelectorAll(".ryan");
+    let i;
+    for(i = 0; i< ryan.length; i++){
+        ryan[i].style.backgroundColor = "";
+    }
+})
 
